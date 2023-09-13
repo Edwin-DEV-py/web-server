e3c593b74c3b26811b798de83862ed52db61dcf9
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2023-09-13 12:55:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ app.post('/enviar-token', (req, res) => {
         }
         const userID = decoded.user_id;
         const {id_carta, price,nombre_carta} = req.body;
-        //const postURL = `http://104.40.30.239:8001/api/cartshop/`;
-        const postURL = 'http://127.0.0.1:8001/api/cartshop/';
+        const postURL = `http://104.40.30.239:8001/api/cartshop/`;
+        //const postURL = 'http://127.0.0.1:8001/api/cartshop/';
         console.log(userID)
         const cartItem = {
             id_carta: id_carta,
@@ -94,8 +94,8 @@ app.post('/remover-carta', (req, res) => {
         }
         const userID = decoded.user_id;
         const {id_carta} = req.body;
-        //const postURL = `http://104.40.30.239:8001/api/cart/remove/`;
-        const postURL = 'http://127.0.0.1:8001/api/cart/remove/';
+        const postURL = `http://104.40.30.239:8001/api/cart/remove/`;
+        //const postURL = 'http://127.0.0.1:8001/api/cart/remove/';
         console.log(userID)
         const cartItem = {
             id_carta: id_carta,
@@ -129,8 +129,8 @@ app.post('/borrar-carta', (req, res) => {
         }
         const userID = decoded.user_id;
         const {id_carta} = req.body;
-        //const postURL = `http://104.40.30.239:8001/api/cart/delete/`;
-        const postURL = 'http://127.0.0.1:8001/api/cart/delete/';
+        const postURL = `http://104.40.30.239:8001/api/cart/delete/`;
+        //const postURL = 'http://127.0.0.1:8001/api/cart/delete/';
         console.log(userID)
         const cartItem = {
             id_carta: id_carta,
@@ -163,8 +163,8 @@ app.get('/obtener-carrito', async(req, res) => {
             return res.status(401).json({ message: 'Token inválido' });
         }
         const userID = decoded.user_id;
-        //const postURL = `http://104.40.30.239:8001/api/cart/`;
-        const postURL = `http://127.0.0.1:8001/api/cart/`;
+        const postURL = `http://104.40.30.239:8001/api/cart/`;
+        //const postURL = `http://127.0.0.1:8001/api/cart/`;
         const cartItem = {
             user: userID
         }
